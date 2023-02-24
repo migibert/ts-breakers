858052f4d46c06782caeae70fd89ce764771a461
Mikael Gibert <mgibert@meilleursagents.com>
2023-02-24 17:58:00 +0100

    fix: ES6 compatibility

--- src/CircuitBreakerError.ts
+++ src/CircuitBreakerError.ts
@@ -1,7 +1,6 @@
 class CircuitBreakerError extends Error {
     constructor(message: string) {
         super(message);
-        Object.setPrototypeOf(this, CircuitBreakerError.prototype);
     }
 }
 
